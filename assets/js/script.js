74272ec5b1227b95bb52d2bd4059fbcd8fc1a11a
muse-jayjay23 <joshuamjones23@hotmail.com>
2022-11-01 16:07:49 +0000

    Add placeholder js to check all the functions are working correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// Allow DOM content to load first so that the function will only apply when all content is loaded in
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = this.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    } 
+});
+
+
+
 function runGame() {
 
 };
@@ -27,5 +46,5 @@ function displaySubtractQuestion() {
 };
 
 function displayMultiplyQuestion() {
-    
+
 };
