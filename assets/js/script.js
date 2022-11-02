b2dd5164f452f0c56d616371b156933fd018609f
muse-jayjay23 <joshuamjones23@hotmail.com>
2022-11-02 11:49:01 +0000

    Add correct division symbol for the question

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,7 +99,7 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if (operator === "%") {
+    } else if (operator === "÷") {
         return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
@@ -150,5 +150,5 @@ function displayMultiplyQuestion(operand1, operand2) {
 function displayDivisionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1 * operand2;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "%";
+    document.getElementById('operator').textContent = "÷";
 }
